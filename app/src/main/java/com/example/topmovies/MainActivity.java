fa2418483dfd48b12d755a04ea229daf2266b54f
Sartikainen <892907@mail.ru>
2025-01-21 16:30:22 +0400

    add MovieAdapter

--- app/src/main/java/com/example/topmovies/MainActivity.java
+++ app/src/main/java/com/example/topmovies/MainActivity.java
@@ -1,14 +1,8 @@
 package com.example.topmovies;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -22,19 +16,19 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
+    private RecyclerView rvPosters;
     private MovieAdapter movieAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        recyclerView = findViewById(R.id.rvPosters);
+        rvPosters = findViewById(R.id.rvPosters);
+        rvPosters.setLayoutManager(new GridLayoutManager(this, 2));
         movieAdapter = new MovieAdapter();
         JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 1);
         ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
-        movieAdapter.addMovies(movies);
-        recyclerView.setAdapter(movieAdapter);
+        movieAdapter.setMovies(movies);
+        rvPosters.setAdapter(movieAdapter);
     }
 }
